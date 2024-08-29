bb86a22f02ca30d4dbe0ef55f5a4aaf0a9c5954c
Lyndz456 <lydiah.wangaru@strathmore.edu>
2024-08-29 17:41:16 +0300

    API

---
+++ conditional.js
@@ -0,0 +1,96 @@
+/*let num= 14;
+
+if(num>4){
+    console.log('Greater than 4');
+}
+else{
+    console.log('Less than 4');
+}
+
+let isAdmin = false;
+
+if (isAdmin){
+    console.log("Admin");
+    callAdminPermisssions(ID);
+}
+else {
+    console.log("Not admin");
+    callNormalPermission(ID);
+}
+
+
+let num = 9
+
+if (num === 1){
+    console.log("One");
+}
+else if (num ===2){
+    console.log("Two");
+
+}
+else if (num ===3){
+    console.log("Three");
+}
+else{
+    console.log("Not valid!");
+}
+
+
+
+let color="green";// red, or whatever
+
+switch(color){
+    case 'blue':
+        console.log("Blue");
+        break;
+     case 'red':
+        console.log("red");
+        break;
+        case 'yellow':
+            console.log("yellow");
+            break;
+            case 'green':
+                console.log("green");
+                break;
+            default:
+                console.log("Not valid");
+                break;
+}
+                
+               for (initialization;condition; incr/decre){
+
+                code
+               }
+               
+fruits= ['apple ', 'banana' , 'mango' , 'orange' , 'grapes'];
+for(let counter =0; counter < fruits.length; counter++){// inorder to not have the last output as undefined,remove the equal sign before the fruits length
+    console.log(fruits[counter]);
+}
+console.log(fruits.length);
+
+*/
+/*
+you have to initialize your variable beforre
+while(condition_is_true){
+    code
+}
+    */
+let j=0;
+while(j<5){
+    console.log(j);
+    j++;
+}
+
+let s=10;
+while(s>1){
+    console.log(s);
+    s--;
+}
+console.log ("outside while loop");
+
+let k=9;
+do{
+    console.log(k);
+    k--;
+}
+while(k>1);
---
+++ function.js
@@ -0,0 +1,39 @@
+/*function hello(){
+    console.log("Hello WOrlds");
+}
+// for us to output somtheing froma function you have to call the nfunction
+hello();
+
+
+function login(name){// function with a purpose and a parametarised function
+    console.log("Welcome" +name);
+}
+
+login("Lydiah")\
+*/
+//function that receives valiues and returns something back
+// function has an error
+let ans = add(20,19);
+console.log(ans);
+
+console.log(add(10,20));
+
+/*
+//anonymous function
+const get_s = function (){
+    console.log ("API Call");
+    return "Hello"; 
+
+} 
+console.log(get_s);
+get_s();
+*/
+
+/*
+// arrow function that can help simplify a function e.g the above function
+function add(a,b){
+    return a+b;
+}
+const add1 =(a, b) =>a + b
+console.log(add1(10,20));
+*/
